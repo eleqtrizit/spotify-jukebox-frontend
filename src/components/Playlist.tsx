f476bc8de8c0320de95c3a7d3361b5ea631973c8
Agustin Rivera <arivera@arivera-mlt.client.nvidia.com>
2022-10-11 15:41:48 -0700

    Fixed resource issue with websockets.

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -2,16 +2,26 @@ import { useEffect, useRef, useState } from 'react';
 import { Track } from './model';
 import TrackArtists from './TrackArtists';
 
+// https://stackoverflow.com/questions/71936110/correct-way-of-connecting-websocket-events-to-update-my-react-component
+
 const Playlist = () => {
     const [serverMessage, setServerMessage] = useState('');
     const [webSocketReady, setWebSocketReady] = useState(false);
     const [tracks, setTracks] = useState<Track[]>([]);
 
-    const [webSocket, setWebSocket] = useState(
-        new WebSocket('ws://127.0.0.1:8000/ws_playlist_tracks/' + localStorage.party_id)
-    );
-
     useEffect(() => {
+        let webSocket = new WebSocket('ws://127.0.0.1:8000/ws_playlist_tracks/' + localStorage.party_id);
+
+        // if (window.performance) {
+        //     if (webSocketReady && performance.navigation.type === 1) {
+        //         console.log('This page is reloaded');
+        //         webSocket.close();
+        //         setWebSocketReady(false);
+        //         webSocket = new WebSocket('ws://127.0.0.1:8000/ws_playlist_tracks/' + localStorage.party_id);
+        //         setWebSocketReady(true);
+        //     }
+        // }
+
         webSocket.onopen = (event) => {
             setWebSocketReady(true);
             webSocket.send('start');
@@ -20,46 +30,58 @@ const Playlist = () => {
         webSocket.onmessage = function (event) {
             setServerMessage(JSON.parse(event.data));
             let data = JSON.parse(event.data);
-            console.log('set tracks');
-            setTracks(data);
+            if (!('type' in data) && !('no_change' in data)) {
+                setTracks(data);
+            } else {
+                console.log('no change');
+            }
         };
 
         webSocket.onclose = function (event) {
             setWebSocketReady(false);
-            setTimeout(() => {
-                setWebSocket(new WebSocket('ws://127.0.0.1:3000/ws'));
-            }, 1000);
+            webSocket.close();
         };
 
         webSocket.onerror = function (err) {
             console.log('Socket encountered error');
             setWebSocketReady(false);
             webSocket.close();
+            webSocket = new WebSocket('ws://127.0.0.1:8000/ws_playlist_tracks/' + localStorage.party_id);
+            setWebSocketReady(true);
         };
 
         return () => {
             webSocket.close();
         };
-    }, [webSocket]);
+    }, []);
 
     if (!webSocketReady) {
-        return <h1>Loading playlist ...</h1>;
-    } else if (serverMessage == '') {
+        return <h1>Connecting...</h1>;
+    } else if (serverMessage === '') {
         return <h1>Loading playlist ...</h1>;
     } else {
-        return (
-            <>
-                <h1>Jukebox</h1>
+        if (tracks.length === 0) {
+            return (
                 <div>
-                    {tracks.map((track) => (
-                        <div className="track" key={track.id}>
-                            <div className="name">{track.name}</div>
-                            <TrackArtists artists={track.artists} />
-                        </div>
-                    ))}
+                    <h1>Jukebox</h1>
+                    <h2>Playlist is empty</h2>
                 </div>
-            </>
-        );
+            );
+        } else {
+            return (
+                <>
+                    <h1>Jukebox</h1>
+                    <div className="playlist_table">
+                        {tracks.map((track) => (
+                            <div className="track" key={track.id}>
+                                <div className="name">{track.name}</div>
+                                <TrackArtists artists={track.artists} />
+                            </div>
+                        ))}
+                    </div>
+                </>
+            );
+        }
     }
 };
 

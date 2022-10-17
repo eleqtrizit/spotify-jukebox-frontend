51aeac0d0357dc0aa2c4f298c7fe5468d08d50b1
Agustin Rivera <arivera@arivera-mlt.client.nvidia.com>
2022-10-17 14:12:33 -0700

    callback update

--- src/components/ForwardCallback.ts
+++ src/components/ForwardCallback.ts
@@ -9,7 +9,7 @@ const ForwardCallback = (code: string | null) => {
         const resData = res.data as PartyId;
         console.log(resData.party_id);
         localStorage.setItem('party_id', resData.party_id);
-        window.location.href = `${process.env.REACT_APP_CALLBACK}/jukebox`;
+        window.location.href = `${process.env.REACT_APP_CALLBACK}/jukebox/${resData.party_id}`;
     };
     if (code && !(code in codes)) {
         codes[code] = true;
